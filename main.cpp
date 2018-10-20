958f0f30d14747de6f1a9778dbc120b86e0e18ee
JoshuaSantillan <37349382+JoshuaSantillan@users.noreply.github.com>
2018-10-20 13:50:44 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 /**
-
- Description:
-Filename:
-Author:
-Date:
+Description: Gets a string and character from user, using a for loop detects how many time the character was used in the string.
+Filename: main.cpp
+Author: Joshua Santillan
+Date: 10/20/18
 */
 
 #include<iostream>
@@ -12,30 +11,46 @@ using namespace std;
 
 
 string get_string(string prompt);
-
+int char_count(string, char );
+char get_char (string prompt);
 int main()
 {
+    string sentance = get_string("Enter a Sentance about anything and everything: ");
+    char c = get_char("Enter a character");
+  
+    cout << "The character you picked is: " << "\"" << c << "\"" << endl;
+    cout << "That character appeared " << char_count(sentance, c) << " time(s)"; 
 
-
-   string sentance = get_string("Enter a Sentance about anything and everything: ");
-   cout << sentance << endl;
-   string count_char = get_string("Enter ONE character to look for in your sentance(may include spaces): ");
-
-   cout << "The Character you picked is: \""+count_char+ "\"" << endl;
    return 0;
-
 }
 
 
 string get_string(string prompt)
 {
-
     string value;
     cout << prompt;
     getline(cin,value);    
-
-   return value;
-
+    return value;
 }
 
+char get_char (string prompt)
+{
+  char c;
+  cout << prompt;
+  cin >> c;
+  return c;
+}
 
+int char_count(string sentance, char c)
+{
+  int count = 0;
+  for (int i = 0; i < sentance.size();i++)
+  {
+    char character = sentance.at(i);    
+      if (character == c)
+      {
+        count ++;
+      }       
+  }
+  return count;
+}
